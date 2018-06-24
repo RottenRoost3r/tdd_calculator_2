984ea866b6c5d0c4e30eb245bebf6f92c17a3bab
Jordan Endicott <jordan.endicott823@gmail.com>
2018-06-24 17:53:05 -0400

    pass add function adds 1 and 1

---
+++ tdd_calculator_2.rb
@@ -0,0 +1,3 @@
+def add_funk(a, b)
+	a+b 
+end
--- test_tdd_calculator_2.rb
+++ test_tdd_calculator_2.rb
@@ -6,4 +6,8 @@ class TestCalculator2 < Minitest::Test
     def test_assert_that_1_equals_1
         assert_equal(1, 1)
     end
+    
+    def test_assert_that_one_plus_one_is_two
+    	assert_equal(2, add_funk(1, 1))
+    end
 end
